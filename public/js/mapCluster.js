938413a40cb5c7769a26e6f65600adf5ccbad6b3
k-kmw <kimmw2_@naver.com>
2022-09-01 16:13:45 +0900

    modify map position

--- public/js/mapCluster.js
+++ public/js/mapCluster.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
 container: 'cluster-map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/light-v10',
-center: [-103.5917, 40.6699],
+center: [127, 37],
 zoom: 3
 });
 map.addControl(new mapboxgl.NavigationControl());
